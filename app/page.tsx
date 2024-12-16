8255cdcc15c788faf4f479fb5e795535556c711a
Felix Trau <rynzaro44@gmail.com>
2024-12-16 12:31:15 +0100

    fix client side error

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ import { Html5QrcodeScanner } from "html5-qrcode";
 
 export default function Home() {
   const [scannedCode, setScannedCode] = useState<string | null>(null);
-  const scannerContainerRef = useRef(null);
+  const scannerContainerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     if (!scannerContainerRef.current) return;
     const scanner = new Html5QrcodeScanner(
-      scannerContainerRef.current,
+      scannerContainerRef.current.id,
       {
         fps: 10,
         qrbox: 250,
@@ -32,7 +32,7 @@ export default function Home() {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center  bg-blue-100 gap-4">
       <div className="text-3xl ">QR Code Scanner</div>
-      <div ref={scannerContainerRef}></div>
+      <div ref={scannerContainerRef} id="scanner-container"></div>
       {scannedCode ? (
         <div>Scanned Result: {scannedCode}</div>
       ) : (
